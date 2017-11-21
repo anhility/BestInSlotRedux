6ef321b408e2128704505f1846e4b284202f351e
anhility <anhility@gmail.com>
2017-11-21 02:34:50 +0100

    Antorus module added

--- Modules/LegionModule/AntorusTheBurningThrone.lua
+++ Modules/LegionModule/AntorusTheBurningThrone.lua
@@ -1,4 +1,4 @@
-local AntorusTheBurningThrone = LibStub("AceAddon-3.0"):GetAddon("BestInSlot"):NewModule("AntorusTheBurningThrone")
+local AntorusTheBurningThrone = LibStub("AceAddon-3.0"):GetAddon("BestInSlotRedux"):NewModule("AntorusTheBurningThrone")
 local AtBT = "AtBT"
 function AntorusTheBurningThrone:OnEnable()
   --local L = LibStub("AceLocale-3.0"):GetLocale("BestInSlotRedux")
