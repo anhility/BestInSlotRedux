f302aa8eb53ecdce553d870e6e9df8c2be6fff9c
anhility <anhility@gmail.com>
2017-11-23 00:21:26 +0100

    Added class names to specc list

--- Modules/LegionModule/LegionArtifact.lua
+++ Modules/LegionModule/LegionArtifact.lua
@@ -65,40 +65,63 @@ function Artifact:OnEnable()
     },
   }
   local itemids = {
+    -- Mage
     [62] = {ids = 127857, relics = {"ARCANE", "FROST", "ARCANE"}},--Arcane
     [63] = {ids = {128820, 133959}, relics = {"FIRE", "ARCANE", "FIRE"}},--Fire
     [64] = {ids = 128862, relics = {"FROST", "ARCANE", "FROST"}},--Frost
+    
+    -- Paladin
     [65] = {ids = {128823, 128824}, relics = {"HOLY", "LIFE", "HOLY"}},--Holy
     [66] = {ids = {128867, 128866}, relics = {"HOLY", "IRON", "ARCANE"}},--Protection
     [70] = {ids = 120978, relics = {"HOLY", "FIRE", "HOLY"}},--Retribution
+    
+    -- Warrior
     [71] = {ids = 128910, relics = {"IRON", "BLOOD", "SHADOW"}},--Arms
     [72] = {ids = {128908, 134553}, relics = {"FIRE", "WIND", "IRON"}},--Fury
     [73] = {ids = {128288, 128289}, relics = {"IRON", "BLOOD", "FIRE"}},--Protection
+    
+    -- Druid
     [102] = {ids = 128858, relics = {"ARCANE", "LIFE", "ARCANE"}}, --Balance
     [103] = {ids = {128860, 128859}, relics = {"FROST", "BLOOD", "LIFE"}}, --Feral
     [104] = {ids = {128821, 128822}, relics = {"FIRE", "BLOOD", "LIFE"}}, --Guardian
     [105] = {ids = 128306, relics = {"LIFE", "FROST", "LIFE"}}, --Restoration
+    
+    -- Death Knight
     [250] = {ids = 128402, relics = {"BLOOD", "SHADOW", "IRON"}}, -- Blood
     [251] = {ids = {128292, 128293}, relics = {"FROST", "SHADOW", "FROST"}}, -- Frost
     [252] = {ids = 128403, relics = {"FIRE", "SHADOW", "BLOOD"}}, -- Unholy
+    
+    -- Hunter
     [253] = {ids = 128861, relics = {"WIND", "ARCANE", "IRON"}}, --Beast Mastery
     [254] = {ids = 128826, relics = {"WIND", "BLOOD", "LIFE"}}, --Marksmanship
     [255] = {ids = 128808, relics = {"WIND", "IRON", "BLOOD"}}, --Survival
+    
+    -- Priest
     [256] = {ids = 128868, relics = {"HOLY", "SHADOW", "HOLY"}}, -- Discipline
     [257] = {ids = 128825, relics = {"HOLY", "LIFE", "HOLY"}}, -- Holy
     [258] = {ids = {128827, 133958}, relics = {"SHADOW", "BLOOD", "SHADOW"}}, -- Shadow
+    
+    -- Rogue
     [259] = {ids = {128870, 128869}, relics = {"SHADOW", "IRON", "BLOOD"}}, -- Assassination
     [260] = {ids = {128872, 134552}, relics = {"BLOOD", "IRON", "WIND"}}, -- Outlaw
     [261] = {ids = {128476, 128479}, relics = {"FEL", "SHADOW", "FEL"}}, -- Subtlety
+    
+    -- Shaman
     [262] = {ids = {128935, 128936}, relics = {"WIND", "FROST", "WIND"}}, -- Elemental
     [263] = {ids = {128819, 128873}, relics = {"FIRE", "IRON", "WIND"}}, -- Enhancement
     [264] = {ids = {128911, 128934}, relics = {"LIFE", "FROST", "LIFE"}}, -- Restoration
+    
+    -- Warlock
     [265] = {ids = 128942, relics = {"SHADOW", "BLOOD", "SHADOW"}}, -- Affliction
     [266] = {ids = 128943, relics = {"SHADOW", "FIRE", "FEL"}}, -- Demonology
     [267] = {ids = 128941, relics = {"FEL", "FIRE", "FEL"}}, -- Destruction
+    
+    -- Monk
     [268] = {ids = 128938, relics = {"LIFE", "WIND", "IRON"}}, -- Brewmaster
     [269] = {ids = {128940, 133948}, relics = {"WIND", "IRON", "WIND"}}, -- Windwalker
     [270] = {ids = 128937, relics = {"FROST", "LIFE", "WIND"}}, -- Mistweaver
+    
+    -- Demon Hunter
     [577] = {ids = {127829, 127830}, relics = {"FEL", "SHADOW", "FEL"}}, -- Havoc
     [581] = {ids = {128832, 128831}, relics = {"IRON", "ARCANE", "FEL"}}, -- Vengeance
   }
